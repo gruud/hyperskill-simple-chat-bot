f10e280e2cfe3db7ea8e690eedd2c63ce1f624e4
gruud <6158418+gruud@users.noreply.github.com>
2023-12-12 10:42:35 +0100

    Project Step 1

---
+++ chatbot.py
@@ -0,0 +1,13 @@
+
+
+def main():
+    """ Main program."""
+    bot_name = "Bacchus"
+    birth_year = 2023
+
+    print(f"Hello! My name is {bot_name}.")
+    print(f"I was created in {birth_year}.")
+
+
+if __name__ == "__main__":
+    main()
