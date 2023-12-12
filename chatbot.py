ca9143a226e90625c7afc45b45e3fc6b9576a11c
gruud <6158418+gruud@users.noreply.github.com>
2023-12-12 10:54:34 +0100

    Project Step 3

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,4 @@
-"""Hyperskill Simple Chat Bot implementation - Step 2"""
+"""Hyperskill Simple Chat Bot implementation - Step 3"""
 
 def main():
     """ Main program."""
@@ -10,6 +10,13 @@ def main():
 
     user_name = input("Please, remind me your name.\n")
     print(f"What a great name you have, {user_name}!")
+
+    print("Let me guess your age.")
+    remainder3 = int(input("Enter remainders of dividing your age by 3, 5 and 7."))
+    remainder5 = int(input())
+    remainder7 = int(input())
+    age = (int(remainder3) * 70 + int(remainder5) * 21 + int(remainder7) * 15) % 105
+    print(f"\nYour age is {age}; that's a good time to start programming!")
 
 
 if __name__ == "__main__":
